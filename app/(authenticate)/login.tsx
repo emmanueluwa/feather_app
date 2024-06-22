8b81505fab4fa1dd8633c87572078f7af316386a
emmanueluwa <emmanuel@fulodev.com>
2024-06-22 21:11:00 +0100

    creating authentication pages

--- app/(authenticate)/login.tsx
+++ app/(authenticate)/login.tsx
@@ -1,11 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const login = () => {
   return (
-    <View>
-      <Text>login</Text>
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <View style={{ height: 200, backgroundColor: "pink", width: "100%" }}>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 25,
+          }}
+        >
+          <Image
+            style={{ width: 150, height: 80, resizeMode: "contain" }}
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/128/6655/6655045.png",
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            marginTop: 20,
+            textAlign: "center",
+            fontSize: 20,
+            fontFamily: "GillSans-SemiBold",
+          }}
+        >
+          Birds of a Feather
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 

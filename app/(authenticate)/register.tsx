abbef744049d4fc220ed4f93fa8dd463b4657510
emmanueluwa <emmanuel@fulodev.com>
2024-06-23 14:29:55 +0100

    adding register screen

--- app/(authenticate)/register.tsx
+++ app/(authenticate)/register.tsx
@@ -1,14 +1,91 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import CustomKeyAvoidingView from "@ui/CustomKeyAvoidingView";
+import WelcomeHeader from "@ui/WelcomeHeader";
+import FormInput from "@ui/FormInput";
+import { useRouter } from "expo-router";
 
 const register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>register</Text>
-    </View>
+    <CustomKeyAvoidingView>
+      <View style={styles.innerContainer}>
+        <WelcomeHeader />
+
+        <View style={styles.formContainer}>
+          <FormInput
+            placeholder="Name"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+
+          <FormInput
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+
+          <FormInput
+            placeholder="Password"
+            secureTextEntry
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+
+          <View style={{ marginTop: 50 }} />
+
+          <Pressable
+            style={{
+              width: 200,
+              backgroundColor: "#FFC0CB",
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+              padding: 15,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              Register
+            </Text>
+          </Pressable>
+
+          <Pressable
+            onPress={() => router.replace("/login")}
+            style={{ marginTop: 12 }}
+          >
+            <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+              Already have an account? Sign in
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </CustomKeyAvoidingView>
   );
 };
 
 export default register;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  innerContainer: {
+    padding: 15,
+    flex: 1,
+  },
+
+  formContainer: { marginTop: 30 },
+});
